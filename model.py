54d393ef8ed7f06b0378506e063e329e49509d3e
shahrukhx01 <sk28671@gmail.com>
2021-04-25 01:25:36 +0000

    feat: add model class

--- model.py
+++ model.py
@@ -1,5 +1,17 @@
+from transformers import BertModel, BertConfig
 
 
 
 class BERTClassifier:
-    
+    def __init__(self, num_labels=2):
+        self.configuration = BertConfig()
+
+    def get_model(self):
+        """
+        Initialize pretrained bert model from huggingface model hub
+        """
+        # initializing a model from the bert-base-uncased style configuration
+        model = BertModel(self.configuration)
+
+        self.model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+        self.model.cuda()
